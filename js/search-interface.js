7bd9089ee7540b4d3fa47a1ab95cf87c0cf48e19
Stephen Emery <emery.stephen@gmail.com>
2016-10-14 08:40:08 -0700

    Add basic JQuery functionality to request repos.

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,1 +1,11 @@
-var apiKey = require('./../.env').apiKey;
+var Search = require('./../js/search.js').searchModule;
+
+$(document).ready(function() {
+  var newSearch = new Search();
+
+  $('#searchButton').click(function(event){
+    event.preventDefault();
+
+    newSearch.getRepos();
+  });
+});
