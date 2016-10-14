0365365c56cd6bcdf2bdbbae1eb11804ef22289b
Stephen Emery <emery.stephen@gmail.com>
2016-10-14 10:25:18 -0700

    Update getRepos and printRepos functions.

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,13 +1,23 @@
 var Search = require('./../js/search.js').searchModule;
 
 var printDetails = function(avatar_url, name, login, html_url, location, public_repos, repos_url, followers) {
-  $('#userDetails').html(
+  $('#userDetails').append(
     '<a href="'+html_url+'"><img src="'+avatar_url+'" alt="User Avatar" id="userImage" /></a>'+
     '<h2>'+name+'</h2>' +
     '<h4>'+login+'</h4>' +
     '<h4>Located in: '+location+'</h4>' +
     '<a href="'+repos_url+'"><h5>Total repos: '+public_repos+'</h5></a>' +
     '<h5># of Followers: '+followers+'</h5>'
+  );
+};
+
+var printRepos = function(name, html_url, description, language){
+  $('#userDetails').append(
+    '<div class="userRepo">'+
+      '<a href="'+html_url+'"><h3>'+name+'</h3></a>'+
+      '<h4>Description: '+description+'</h4>'+
+      '<h4>Most Used Langauge: '+language+'</h4>'+
+    '</div>'
   );
 };
 
@@ -19,6 +29,6 @@ $(document).ready(function() {
     var username = $('#username').val();
 
     newSearch.getDetails(username, printDetails);
-    newSearch.getRepos(username);
+    newSearch.getRepos(username, printRepos);
   });
 });
--- js/search.js
+++ js/search.js
@@ -7,21 +7,17 @@ function Search() {
 
 Search.prototype.getDetails = function (username, printDetails) {
   $.get('https://api.github.com/users/'+username+'?access_token=' + apiKey).then(function(response){
-    console.log(response);
     printDetails(response.avatar_url, response.name, response.login, response.html_url, response.location, response.public_repos, response.repos_url, response.followers);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
 
-Search.prototype.getRepos = function (username) {
+Search.prototype.getRepos = function (username, printRepos) {
   $.get('https://api.github.com/users/'+username+'/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
     for (var repo of response) {
-      console.log(repo.name);
-      console.log(repo.html_url);
-      console.log(repo.description);
-      console.log(repo.language);
+      printRepos(repo.name, repo.html_url, repo.description, repo.language);
       console.log(repo.created_at);
     }
   }).fail(function(error){
