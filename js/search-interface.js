e345e4c6f67450ad3db32afab2a4ce2e3b0a57e9
Stephen Emery <emery.stephen@gmail.com>
2016-10-14 10:54:50 -0700

    Clean up javascript functions.

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,12 +1,12 @@
 var Search = require('./../js/search.js').searchModule;
 
-var printDetails = function(avatar_url, name, login, html_url, location, public_repos, repos_url, followers) {
+var printDetails = function(avatar_url, name, login, html_url, location, public_repos, followers) {
   $('#userDetails').html(
     '<a href="'+html_url+'"><img src="'+avatar_url+'" alt="User Avatar" id="userImage" /></a>'+
     '<h2>'+name+'</h2>' +
     '<h4>'+login+'</h4>' +
     '<h4>Located in: '+location+'</h4>' +
-    '<a href="'+repos_url+'"><h5>Total repos: '+public_repos+'</h5></a>' +
+    '<h5>Total repos: '+public_repos+'</h5>' +
     '<h5># of Followers: '+followers+'</h5>'
   );
 };
--- js/search.js
+++ js/search.js
@@ -7,9 +7,8 @@ function Search() {
 
 Search.prototype.getDetails = function (username, printDetails, detailsError) {
   $.get('https://api.github.com/users/'+username+'?access_token=' + apiKey).then(function(response){
-    printDetails(response.avatar_url, response.name, response.login, response.html_url, response.location, response.public_repos, response.repos_url, response.followers);
+    printDetails(response.avatar_url, response.name, response.login, response.html_url, response.location, response.public_repos, response.followers);
   }).fail(function(error){
-    console.log(error.responseJSON.message);
     detailsError();
   });
 };
@@ -26,10 +25,8 @@ Search.prototype.getRepos = function (username, printRepos, reposError) {
         language = "No Language";
       }
       printRepos(repo.name, repo.html_url, description, language);
-      console.log(repo.created_at);
     }
   }).fail(function(error){
-    console.log(error.responseJSON.message);
     reposError();
   });
 };
